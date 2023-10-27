abf308dde5ce4018096c6eb1620309591edc2b14
Sai Manikanta Andey <saiandeyedu007@gmail.com>
2023-10-27 20:28:30 +0530

    progress day 2

--- src/components/AboutUS/About.jsx
+++ src/components/AboutUS/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Users from '../UsesrInAbout/Users'
+import './About.css'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+    <div className='about-container'>
+      <h2 className='title'>About The Team</h2>
+      <Users/>
+    </div>
+  );
 }
 
 export default About
---
+++ src/components/UsesrInAbout/Users.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Users.css";
+
+const Users = () => {
+  return (
+    <div className='users-container'>
+        <h3>Name : Sai Manikanta Andey</h3>
+        <h4>Stack : React, Redux Toolkit, RWD, MySQL</h4>
+    </div>
+  )
+}
+
+export default Users
